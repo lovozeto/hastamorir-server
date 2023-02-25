3835a123b3a0cd77cfb9750e5cd6157d897a4cc3
Esteban Loboguerrero <accounts@lovozeto.com>
2023-02-25 15:55:04 -0500

    Create install_combustion.sh

---
+++ tools/transmission/install_combustion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Download latest combustion release
+wget https://github.com/Secretmapper/combustion/archive/release.zip -O /tmp/combustion.zip
+
+# Purge placeholder files
+rm -r combustion-release/*
+
+# Extract files to combustion folder
+unzip /tmp/combustion.zip -d ~/.combustion
+
+# Point environment variable to combustion folder
+# export TRANSMISSION_WEB_HOME="$HOME/.combustion/combustion-release"
+
+# Add reboot entry
+@reboot /install_combustion.sh
