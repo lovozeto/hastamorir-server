59bd7364c85f5b2c5ca663a482de9f8c0fc07050
Esteban Loboguerrero <accounts@lovozeto.com>
2023-03-24 16:29:54 -0500

    Update install_combustion.sh

--- tools/transmission/install_combustion.sh
+++ tools/transmission/install_combustion.sh
@@ -9,8 +9,5 @@ rm -r combustion-release/*
 # Extract files to combustion folder
 unzip /tmp/combustion.zip -d /
 
-# Point environment variable to combustion folder
-# export TRANSMISSION_WEB_HOME="$HOME/.combustion/combustion-release"
-
 # Add reboot entry
 @reboot /install_combustion.sh
