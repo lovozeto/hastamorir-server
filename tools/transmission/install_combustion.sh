bec4989e2121a4b6ba42560ebd10aaa2a1b87958
Esteban Loboguerrero <accounts@lovozeto.com>
2023-11-02 02:47:00 +0000

    Updates scripts

--- tools/transmission/install_combustion.sh
+++ tools/transmission/install_combustion.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Download latest combustion release
-wget https://github.com/Secretmapper/combustion/archive/release.zip -O /tmp/combustion.zip
+# Get the latest version of combustion UI.
+latest_version=$(curl -sL https://api.github.com/repos/combustion-ui/combustion-ui/releases/latest | jq -r '.tag_name')
 
-# Purge placeholder files
-rm -r combustion-release/*
+# Download the latest version of combustion UI.
+curl -o combustion-ui.zip https://github.com/combustion-ui/combustion-ui/releases/download/$latest_version/combustion-ui-$latest_version.zip
 
-# Extract files to combustion folder
-unzip /tmp/combustion.zip -d /
+# Unzip the combustion UI archive.
+unzip -o combustion-ui.zip
 
-# Add reboot entry
-@reboot /install_combustion.sh
+# Move the combustion UI directory to the /combustion-release/ directory.
+mv combustion-ui /combustion-release/
+
+# Remove the combustion UI archive.
+rm combustion-ui.zip
