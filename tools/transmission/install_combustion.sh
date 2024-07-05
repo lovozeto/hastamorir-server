b3cc943c311f39e90ba746f773251af2cde3d853
Esteban Loboguerrero <accounts@lovozeto.com>
2024-07-05 23:22:36 +0000

    I hate windows fixes

--- tools/transmission/install_combustion.sh
+++ tools/transmission/install_combustion.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Get the latest version of combustion UI.
-latest_version=$(curl -sL https://api.github.com/repos/combustion-ui/combustion-ui/releases/latest | jq -r '.tag_name')
-
 # Download the latest version of combustion UI.
-curl -o combustion-ui.zip https://github.com/combustion-ui/combustion-ui/releases/download/$latest_version/combustion-ui-$latest_version.zip
+wget https://github.com/Secretmapper/combustion/archive/release.zip
 
 # Unzip the combustion UI archive.
-unzip -o combustion-ui.zip
-
-# Move the combustion UI directory to the /combustion-release/ directory.
-mv combustion-ui /combustion-release/
+unzip -o release.zip
 
 # Remove the combustion UI archive.
-rm combustion-ui.zip
+rm release.zip
